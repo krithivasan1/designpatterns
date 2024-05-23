6253dcb7b8fd4ed411ab901e20eb83ec34992e4d
krithivasan1 <krithi.vn@gmail.com>
2024-05-23 09:07:45 +0530

    Singleton Pattern Example

---
+++ src/main/java/com/example/designpatterns/Singleton/Client.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.Singleton;
+
+import java.sql.SQLOutput;
+
+public class Client {
+    public static void main(String[] args) {
+        DatabaseConnection dbc = DatabaseConnection.getInstance();
+
+        DatabaseConnection dbc1 = DatabaseConnection.getInstance();
+        System.out.println("Debug");
+        // You can see the dbc and dbc1 creates the same object 
+    }
+}
